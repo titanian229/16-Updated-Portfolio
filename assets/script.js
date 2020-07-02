444178aff81f4e0b4ce682736a4dd599daaff811
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-07-01 23:41:23 -0400

    Added more unneeded but really cool animations

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
-gsap.from("#welcome-container h1", { delay: 2, duration: 1, x: 110, opacity: 0, ease: "circ" });
+let tl = gsap.timeline();
+
+tl.from("#welcome-container h1", { delay: 2, duration: 1, x: 110, opacity: 0, ease: "circ" });
 // gsap.from("#welcome-container p", { delay: 3, duration: 1, x: -110, opacity: 0, ease: "circ" });
-gsap.to("#astronaut", { delay: 5, duration: 7, width: 50, ease: "back.inOut", left: 30, top: 30 });
-// gsap.from("#earth", {delay:4, duration: 3, bottom: "-30vh"})
+// tl.to("#astronaut", { delay: 0.5, duration: 3, width: 50, ease: "back.inOut", left: 30, top: 30 });
+tl.to("#earth", {delay:1, duration: 3, bottom: "-30vh"})
+tl.from("nav", { duration: 1.5, opacity: 0, ease: "circ"});
 
 // gsap.to("#about-container img", {delay:3, duration:3, y: 10, opacity: 0});
 
